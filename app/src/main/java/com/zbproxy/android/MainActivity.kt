9840c80d1be18504f3e2a0936624fc1b86e1673a
aeroheaven1 <3144786900@qq.com>
2026-08-20 18:08:07 +0800

    Fix missing stringResource import in MainActivity

--- app/src/main/java/com/zbproxy/android/MainActivity.kt
+++ app/src/main/java/com/zbproxy/android/MainActivity.kt
@@ -15,6 +15,7 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
